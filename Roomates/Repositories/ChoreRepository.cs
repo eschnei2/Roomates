8627704c4b4ffda6e665f5600ecbd858bcc3de1c
eschnei2 <everettschneider@gmail.com>
2021-09-10 17:12:42 -0500

    chores and stuff

---
+++ Roomates/Repositories/ChoreRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Roommates.Models;
+using Microsoft.Data.SqlClient;
+
+namespace Roommates.Repositories
+{
+    class ChoreRepository : BaseRepository
+    {
+        public ChoreRepository(string connectionString) : base(connectionString) { }
+
+        public List<Chore> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Name FROM Chore";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Chore> chores = new List<Chore>();
+
+                    while (reader.Read())
+                    {
+                        int idColumnPosition = reader.GetOrdinal("Id");
+
+                        int idValue = reader.GetInt32(idColumnPosition);
+
+                        int nameColumnPosition = reader.GetOrdinal("Name");
+                        string nameValue = reader.GetString(nameColumnPosition);
+
+                        Chore chore = new Chore
+                        {
+                            Id = idValue,
+                            Name = nameValue
+                        };
+
+                        chores.Add(chore);
+
+                    }
+
+                    reader.Close();
+
+                    return chores;
+                }
+            }    
+        }
+    }
+}
